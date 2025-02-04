49c379ff781d5c22999406c853aa2d9558a00390
Aman-Giri <amangirikg@gmail.com>
2025-02-05 02:40:47 +0530

    addedmore Logics

--- CodingQuestions/AlternateReverse.java
+++ CodingQuestions/AlternateReverse.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class AlternateReverse {
 public static void main(String[] args) {
+    //Do the Alternate reverse for the above String
+     //Also make the first letter of the reversed word as capital
         String input = "Aman Kumar Giri Goswami";
         String[] words = input.split(" ");
         String result = "";
@@ -9,6 +11,7 @@ public static void main(String[] args) {
         for(int i=0;i<words.length;i++){
             if(i%2!=0){
                 revResult = revString(words[i]);
+                revResult = Character.toUpperCase(revResult.charAt(0))+revResult.substring(1);
             }else{
                 revResult = words[i];
             }
@@ -31,5 +34,11 @@ public static void main(String[] args) {
         }
 
         return rev;
+    }
+
+    //Function to reverse the String using StringBuilder
+    public static String revString2(String str){
+        StringBuilder sb = new StringBuilder(str.toLowerCase()).reverse();
+        return sb.toString();
     }
 }
---
+++ CodingQuestions/CheckPrimeNumber.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class CheckPrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: " );
+        int number = sc.nextInt();
+
+        if(isPrime(number) == true){
+            System.out.println(number+" is prime number");
+        }else{
+            System.out.println(number+" is not prime number");
+        }
+    }
+
+    //function to check the prime number: 
+    public static boolean isPrime(int number){
+        if(number < 2) return false; // number less than 2 is not prime and also 2 is the smallest prime number
+        for(int i=2;i*i<=number;i++){
+            if(number%2==0)return false; // not prime
+        }
+        return true; //prime number
+    }
+}
